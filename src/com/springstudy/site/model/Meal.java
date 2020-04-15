24ebad711b508ba54413632b4d03af326635a342
WenDeHor <gaheru20>
2020-04-15 09:07:22 +0300

    1.Start study spring progect

---
+++ src/com/springstudy/site/model/Meal.java
@@ -0,0 +1,27 @@
+package com.springstudy.site.model;
+
+import java.time.LocalDateTime;
+
+public class Meal {
+    private final LocalDateTime dateTime;
+    private final String description;
+    private final int calories;
+
+    public Meal(LocalDateTime dateTime, String description, int calories) {
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+}
